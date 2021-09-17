53bb65f6d8aef66667a2b86094eb12426b5af672
Edvin Boul <kittenparry@users.noreply.github.com>
2021-09-17 23:11:50 +0300

    Create sub-shop categories, example item list for testing
    Shop sub categories are: weapons, characters, armour, potions, boosters, skins for now, just ideas
    Load example JSON file into memory and printing works but turning it into an object hmm...
    Think I found the problem, it gets the object as a single object because it is a single object with sub-objects as ids for headers

--- Assets/Resources/Scripts/ItemShopMenu.cs
+++ Assets/Resources/Scripts/ItemShopMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ItemShopMenu : MonoBehaviour
 {
     public Transform ShopMenu;
+    public Item ItemList;
 
 
     // Start is called before the first frame update
@@ -24,30 +25,92 @@ public class ItemShopMenu : MonoBehaviour
             Debug.Log("No choices selected");
         }
 
-        // Find all children buttons and add onClick
+        LoadShopMenuList();
+
+        // Find all children buttons of main shop and add onClick
         int i = 0;
         foreach (Transform t in ShopMenu)
         {
             int currentIndex = i;
 
             Button b = t.GetComponent<Button>();
-            b.onClick.AddListener(() => OnItemSelect(currentIndex));
+            b.onClick.AddListener(() => OnShopLabelSelect(currentIndex));
 
             i++;
 
         }
     }
 
-    public void OnItemSelect(int currentIndex)
+    public void OnShopLabelSelect(int currentIndex)
     {
         print("Selected item: " + currentIndex);
-        Debug.Log("Selected item: " + currentIndex);
+        
+        switch(currentIndex)
+        {
+            case 0:
+                print("load weapons menu");
+                break;
+            case 1:
+                print("load characters menu");
+                break;
+            case 2:
+                print("load armour menu");
+                break;
+            case 3:
+                print("load potions menu");
+                break;
+            case 4:
+                print("load boosters menu");
+                break;
+            case 5:
+                print("load skins menu");
+                break;
+            default:
+                break;
+
+        }
     }
 
     public void OnBuyButtonClick()
     {
         print("Buy button clicked.");
-        Debug.Log("Buy button clicked.");
     }
 
+    private void populateItemList() // temporary function to fill the shop item list
+    {
+
+    }
+
+
+    [SerializeField]
+    private TextAsset itemList = null;
+
+    public void LoadShopMenuList()
+    {
+        if (itemList != null)
+        {
+            string dataAsJSON = itemList.text;
+            // print(dataAsJSON);
+            ItemList = JsonUtility.FromJson<Item>(dataAsJSON);
+            print(ItemList); // FIXME: how to get data from this?
+        }
+        else
+        {
+            print("No internet connection or something?");
+        }
+    }
+
+}
+
+public class Item
+{
+    private int id;
+    private string name;
+    private string category;
+    private string type;
+    private string bodySlot;
+    private string rarity;
+    private int price;
+    private string icon;
+    private string description;
 }
